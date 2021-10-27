c9ce9fc392f6ae56bc226e41c94b94f5753a9ef6
ishaynitzan <ishaynitzan@gmail.com>
2021-10-27 14:07:03 +0300

    add editor and buttons

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,2 +1,36 @@
 'use strict'
 
+function onInit() {
+    renderGallery();
+    renderKeyWords() 
+}
+
+function renderGallery() {
+
+    const imgs = getImgs();
+    var strHtml = '';
+    var elImgs = imgs.map(img => {
+        strHtml += `<div><img src="./meme-imgs (square)/${img.id}.jpg" class="memeImg" data-id="${img.id}" alt="" onclick="onSelectedImg(this)"></div>`
+    });
+
+    strHtml += elImgs.join('');
+
+    document.querySelector(`.gallery`).innerHTML = strHtml;
+}
+
+function renderKeyWords() {
+
+    const words = getKeyWords();
+    var strHtml = '';
+    for (const key in words) {
+        const size = words[key]%5;
+        strHtml += `<li class="flex size${size}">${key}</li>`;
+    }
+
+    document.querySelector(`.key-words`).innerHTML = strHtml;
+}
+
+
+function onSelectedImg(elImg){
+    selectedImg(elImg.dataset.id);
+}
